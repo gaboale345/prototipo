09898c725f8a91d885313e0a5dd11821f64f2115
gaboale345 <parrameojisa@gmail.com>
2026-07-31 09:39:33 -0400

    fix: set base '/' in vite.config.js for SPA routing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || './',
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
